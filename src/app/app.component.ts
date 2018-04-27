85dbace1c1334fca8a2fbaaa7904937f8ef9d559
Ricardo Saracino <ricardo.saracino@ricardosaracino.com>
2018-04-27 16:02:29 -0400

    messages and ui clean up

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
+import { Meta } from '@angular/platform-browser';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'Tour of Heroes';
+
+  constructor(private meta: Meta) {
+    this.meta.addTag({ name: 'description', content: 'Tour of Heroes' });
+    this.meta.addTag({ name: 'author', content: 'Ricardo Saracino' });
+    this.meta.addTag({ name: 'viewport', content: 'width=device-width, initial-scale=1, shrink-to-fit=no' });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,36 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
-import { HeroesComponent } from './heroes/heroes.component';
+import {NgModule} from '@angular/core';
+
+import {FormsModule} from '@angular/forms'; // <-- NgModel lives here
+
+import {AppComponent} from './app.component';
+
+
+
+import {HeroesComponent} from './heroes/heroes.component';
+
+import { HeroService } from './hero.service';
+import { MessageService } from './message.service';
+import { MessagesComponent } from './messages/messages.component';
+
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeroesComponent
+    HeroesComponent,
+    MessagesComponent
   ],
   imports: [
     BrowserModule,
     FormsModule
   ],
-  providers: [],
+  providers: [
+    HeroService,
+    MessageService
+  ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
