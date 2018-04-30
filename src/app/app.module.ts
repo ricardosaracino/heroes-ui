fd2051e18e2eb67bcb314a36f568ec21729ca4df
Ricardo Saracino Home <RICARDOSARACINO@PROTONMAIL.COM>
2018-04-29 22:39:22 -0400

    intereceptor stuff

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,17 @@ import {NgModule} from '@angular/core';
 
 import {BrowserModule} from '@angular/platform-browser';
 import {FormsModule} from '@angular/forms';
-import {HttpClientModule} from '@angular/common/http';
+import {HTTP_INTERCEPTORS, HttpClientModule, HttpXsrfTokenExtractor} from '@angular/common/http';
+
+
+//https://theinfogrid.com/tech/developers/angular/building-http-interceptor-angular-5/
+/*import {
+  HttpXsrfCookieExtractor,
+  HttpXsrfInterceptor,
+  XSRF_COOKIE_NAME,
+  XSRF_HEADER_NAME
+} from '@angular/common/http/src/xsrf';*/
+
 
 import {AppComponent} from './app.component';
 
@@ -17,6 +27,19 @@ import {MessageService} from './message.service';
 import {MessagesComponent} from './messages/messages.component';
 
 import {AppRoutingModule} from './app-routing.module';
+
+//https://theinfogrid.com/tech/developers/angular/building-http-interceptor-angular-5/
+//https://blog.angularindepth.com/insiders-guide-into-interceptors-and-httpclient-mechanics-in-angular-103fbdb397bf
+/*
+@NgModule({
+  providers: [
+    HttpXsrfInterceptor,
+    {provide: HTTP_INTERCEPTORS, useExisting: HttpXsrfInterceptor, multi: true},
+    {provide: HttpXsrfTokenExtractor, useClass: HttpXsrfCookieExtractor},
+    {provide: XSRF_COOKIE_NAME, useValue: 'connect.sid'},
+    {provide: XSRF_HEADER_NAME, useValue: 'X-XSRF-TOKEN'},
+  ]
+})*/
 
 @NgModule({
   imports: [
